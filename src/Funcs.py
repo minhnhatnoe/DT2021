8d4e7e346996e78f5050146d52b1e70a361ed6e0
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-02-13 14:26:34 +0700

    New Features

---
+++ src/Funcs.py
@@ -0,0 +1,35 @@
+from disnake import Embed
+import json
+import requests
+
+rankcolor = {
+    "newbie": 13421772,
+    "pupil": 7864183,
+    "specialist": 12311927
+}
+
+def getuser(userlist):
+    try:
+        fromnet = requests.get(f"https://codeforces.com/api/user.info?handles={';'.join(userlist)}").text
+    except:
+        raise("Network Error")
+    json_data = json.loads(fromnet)
+    if json_data["status"] == "Failed":
+        raise("Server Error")
+    return json_data["result"]
+
+def userEmbed(handle: str, dischand: str):
+    data = getuser([handle])[0]
+    obj = Embed(title = dischand, color=rankcolor[data["rank"]], description=data["rank"].title())
+    # obj.set_image(url=data["titlePhoto"])
+    obj.set_thumbnail(url=data["avatar"])
+    obj.add_field("Handle", data["handle"])
+    obj.add_field("Name", data["firstName"] + " " + data["lastName"])
+    if "country" in data:
+        obj.add_field("Country", data["country"])
+    if "city" in data:
+        obj.add_field("City", data["city"])
+    if "organization" in data:
+        obj.add_field("Organization", data["organization"])
+    obj.add_field("Rating", data["rating"])
+    return obj
