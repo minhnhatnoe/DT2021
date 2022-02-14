1bb3612d3c705a9e6d734ba63c83902b692c5425
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-02-14 16:53:10 +0700

    Something abt adding roles. It probably will remain incomplete until I have time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import disnake
 from disnake.ext import commands
 import os  # TODO: Minimize footprint of os
 from dotenv import load_dotenv
-
+import json
+import src.Codeforces.Funcs
 load_dotenv()
 
 guilds = [int(v) for v in os.environ.get("TEST_GUILDS").split(", ")]
@@ -28,6 +30,32 @@ async def helpme(inter):
             help_msg.append(cmd.description)
     
     await inter.response.send_message(msg + "```" + "\n".join(help_msg) + "```")
+
+@bot.slash_command()
+async def updateme(inter, user: disnake.User):
+    '''/updateme @<Discord>: Add someone to the handle update list'''
+    load_dotenv()
+    path = os.environ.get("DATAPATH")
+    with open(f"{path}\update.json", "r+") as json_file:
+        json_data = json.load(json_file)
+        json_file.seek(0)
+        json_data[str(inter.guild)].append(str(user.id))
+        json.dump(json_data, json_file)
+        json_file.truncate()
+    await inter.response.send_message(f"{user.mention} has been added to the update list")
+
+@bot.slash_command()
+async def refresh():
+    '''/refresh: Refresh all color-based roles'''
+    path = os.environ.get("DATAPATH")
+    with open(f"{path}\update.json", "r") as json_file:
+        json_data = json.load(json_file)
+        for guildid in json_data:
+            guild = bot.get_guild(int(guildid))
+            for userid in json_data[guild]:
+                user = guild.get_member(int(userid))
+                user.add_roles() # TODO
+    await bot.response.send_message("Refreshed all roles")
 
 @bot.event
 async def on_ready():
