3456ad70ab5a18b89f8c6e0edc22645e36620631
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-03-02 21:28:08 +0700

    refactor

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from disnake.ext import commands
-import os  # TODO: Minimize footprint of os
-from dotenv import load_dotenv
+from src.imports import *
+load_dotenv()
+guilds = [int(v) for v in environ.get("TEST_GUILDS").split(",")]
+bot = commands.Bot(test_guilds=guilds, intents = disnake.Intents.all())
 
-guilds = [934020662765453312]
-bot = commands.Bot(test_guilds=guilds)
+@bot.event
+async def on_guild_join(guild):
+    '''Add the bot to a guild'''
+    await src.sus.makerole(guild)
+
+@bot.event
+async def on_guild_remove(guild: disnake.Guild):
+    '''Remove the bot from a guild'''
+    guildroles = JsonHandler.get_roles(guild.id)
+    if guildroles is None: return
+    for rolename, roleid in guildroles.items():
+        role = await guild.get_role(int(roleid))
+        await role.delete()
+    JsonHandler.remove_guild(guild.id)
 
 @bot.event
 async def on_ready():
     print("Logged in")
 
+@bot.slash_command()
+async def helpme(inter):
+    '''/helpme: Show this help message'''
+    msg = 'Here are several things I can do:'
+
+    command_set = bot.get_guild_slash_commands(inter.guild.id)
+    help_msg = []
+    for cmd in command_set:
+        if cmd.options:
+            for opt in cmd.options:
+                help_msg.append(opt.description)
+        else:
+            help_msg.append(cmd.description)
+    
+    await inter.response.send_message(msg + "```" + "\n".join(help_msg) + "```")
+
 bot.load_extension("src.Codeforces.Commands")
+bot.load_extension("src.General")
 
 if __name__ == "__main__":
     load_dotenv()
-    bot.run(os.environ.get("TOKEN"))
+    bot.run(environ.get("TOKEN"))
--- src/Codeforces/Commands.py
+++ src/Codeforces/Commands.py
@@ -1,10 +1,4 @@
-import json
-import disnake
-import os
-from dotenv import load_dotenv
-from src.Codeforces import Funcs
-from disnake.ext import commands
-
+from src.imports import *
 class CFCommand(commands.Cog):
     "A cog for all of commands regarding Codeforces"
 
@@ -24,31 +18,25 @@ class CFCommand(commands.Cog):
             json_file.truncate()
         await inter.response.send_message(f"{inter.author.mention} has been introduced as {handle}")
 
-    @commands.slash_command()
-    async def query(inter, dischand: disnake.User):
-        '''/query @<Discord>: Get someone's CF handle'''
-        load_dotenv()
-        path = os.environ.get("DATAPATH")
-        with open(f"{path}\handle.json", "r") as json_file:
-            json_data = json.load(json_file)
-            if str(dischand.id) in json_data:
-                await inter.response.send_message(f"{dischand.mention} is {json_data[str(dischand.id)]}", embed=Funcs.userEmbed(json_data[str(dischand.id)], dischand))
-            else:
-                await inter.response.send_message(f"{dischand.mention} has not been introduced yet")
+    @cf.sub_command()
+    async def assign(self, inter, handle: str):
+        '''/cf assign <CF Handle>: Let the bot know your Codeforces handle'''
+        # try:
+        embedobj = await Funcs.CFExternal.get_user_embed(handle, inter.user.id)
+        await inter.response.send_message(f"{inter.author.mention} has been introduced as {handle}", embed = embedobj)
+        Funcs.CFInternal.assign_handle(inter.author.id, handle)
+        # except:
+            # await inter.response.send_message(f"Error occurred. Please carefully check provided handle")
 
-    @commands.slash_command()
-    async def ping(inter):
-        '''/ping: Get the bot's latency'''
-        await inter.response.send_message("Pong!")
-
-    @commands.slash_command()
-    async def helpme(inter):
-        '''/helpme: Get help'''
-        message = '''
-        Here are several things I can do:
-            1. /introduce <CF Handle>: Let the bot know your Codeforces handle
-            2. /query @<Discord>: Get someone's CF handle
-            3. /help: You just used this
-        '''
+    @cf.sub_command()
+    async def info(self, inter, user: disnake.User):
+        '''/cf info @<Discord>: Get someone's CF handle'''
+        handle = Funcs.CFInternal.get_handle(user.id)
+        if handle is None:
+            await inter.response.send_message(f"{user.mention} has not been introduced yet")
+        else:
+            # await inter.response.send_message(f"{user.mention}'s handle is {handle}", embed = await Funcs.getUserEmbed(handle, user.name))
+            await inter.response.send_message(embed = await Funcs.CFExternal.get_user_embed(handle, user.name))
+                
 def setup(bot: commands.Bot):
     bot.add_cog(CFCommand(bot))
