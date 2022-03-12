6d7415e07776b59b22d704c74c33b0f00f7f321e
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-03-12 22:48:27 +0700

    Added groundwork for setting active fetching time

--- src/automatic_submission_fetching.py
+++ src/automatic_submission_fetching.py
@@ -12,10 +12,19 @@ class SubmissionFetching:
         self.destination_channel = channel
         self.contest_id = id
 
+    def set_active_range(self, start_time: int, end_time: int) -> None:
+        pass
+
+    @tasks.loop()
+    async def begin_loop(self) -> None:
         self.fetch.start()
 
+    @tasks.loop()
+    async def end_loop(self) -> None:
+        self.fetch.stop()
+
     @tasks.loop(seconds = 15.0)
-    async def fetch(self):
+    async def fetch(self) -> None:
         print(f"Fetching submissions for contest ID = {self.contest_id}")
         await self.destination_channel.send(content = "Testing...")
 
@@ -30,6 +39,7 @@ class SubmissionFetchingCog(commands.Cog):
     async def setup(self, inter, contest_id: int) -> None:
         '''Setup an instance of SubmissionFetching for fetching'''
         fetch_module = SubmissionFetching(self.bot, inter.channel, int(contest_id))
+        # TODO: Set start_time and end_time, handle: (before contest, in contest, after contest)
         await inter.response.send_message(f"Setup to fetch submission from contest with ID: {contest_id} (Not functioning yet)")
 
 def setup(bot: commands.Bot):
