e1fef0ed9aa4f21505c245c70e8303b0c95b69f5
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-03-12 16:39:14 +0700

    Support multiple fetching at once

--- src/automatic_submission_fetching.py
+++ src/automatic_submission_fetching.py
@@ -3,27 +3,35 @@
 import disnake
 from disnake.ext import commands, tasks
 
-class SubmissionFetching(commands.Cog):
-    '''Cog for fetching submissions'''
+class SubmissionFetching:
+    '''A class for fetching submissions'''
 
-    def __init__(self, bot):
+    def __init__(self, bot, channel, id):
         '''Initialization'''
         self.bot = bot
-        self.destination_channel = None
-        self.contest_id = None
+        self.destination_channel = channel
+        self.contest_id = id
 
-    @commands.slash_command(name = "fetch")
-    async def setup(self, inter, contest_id: int) -> None:
-        self.contest_id = int(contest_id)
-        self.destination_channel = inter.channel
         self.fetch.start()
-        await inter.response.send_message(f"Setup to fetch submission from contest with ID: {contest_id} (Not functioning yet)")
 
-    @tasks.loop(seconds = 10.0)
+    @tasks.loop(seconds = 15.0)
     async def fetch(self):
         print(f"Fetching submissions for contest ID = {self.contest_id}")
         await self.destination_channel.send(content = "Testing...")
 
+class SubmissionFetchingCog(commands.Cog):
+    '''A cog for creating SubmissionFetching instance'''
+
+    def __init__(self, bot):
+        '''Initialization'''
+        self.bot = bot
+
+    @commands.slash_command(name = "fetch")
+    async def setup(self, inter, contest_id: int) -> None:
+        '''Setup an instance of SubmissionFetching for fetching'''
+        fetch_module = SubmissionFetching(self.bot, inter.channel, int(contest_id))
+        await inter.response.send_message(f"Setup to fetch submission from contest with ID: {contest_id} (Not functioning yet)")
+
 def setup(bot: commands.Bot):
     '''Add the cog to the bot'''
-    bot.add_cog(SubmissionFetching(bot))
+    bot.add_cog(SubmissionFetchingCog(bot))
