bdb5e8609af32b78103da487c1f9de75c472e58b
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-06-12 20:20:26 +0700

    Added default params

--- src/utils/startup_check.py
+++ src/utils/startup_check.py
@@ -3,14 +3,24 @@ from disnake.ext import commands
 from src import cfg
 from src.utils import json_file
 
+
 def check_guilds_config() -> bool:
     config = json_file.load_from_json("/server_config.json")
     for guild in cfg.bot.guilds:
         if str(guild.id) not in config:
             choice = input(f"Guild {guild.name} with id {guild.id} has \
 not been configured. Configure with default params? (Y/N)").upper()
-            if choice:
+            if choice == "Y":
                 config[str(guild.id)] = {
-
+                    "roles": {
+                        "codefun": False,
+                        "codeforces": False,
+                        "codechef": False
+                    },
+                    "info": {
+                        "codefun": False,
+                        "codeforces": False,
+                        "codechef": False
+                    }
                 }
     json_file.write_to_json("/server_config.json", config)
